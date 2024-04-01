80611bb8258df47aa4540878007578fdeb4d6ac6
ALYA <dessau505@gmail.com>
2024-04-01 19:51:46 +0300

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+function greet() {
+    alert(Hello);
+}
+function datetime() {
+    alert("25.03.2024 12:25");
+}
+
+// ===mmm===
+
+let temperature = 17;
+let isWindy = true;
+
+if (isWindy) {
+    console.log(Windy);
+}
+else {
+    console.log(There will be no wind);
+}
+if (temperature < 15) {
+    console.log(Dress warmly and take a hat);
+}
+else {
+    console.log(A good day for a walk);
+}
