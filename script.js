70057dd2ac4fbb94b04c85fc4a7ea3cdf103a310
HelloWorld <dessau505@gmail.com>
2024-04-02 15:47:59 +0300

    lesson3

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-function greet() {
-    alert(Hello);
-}
-function datetime() {
-    alert("25.03.2024 12:25");
-}
+// function greet() {
+//     alert("Hello");
+// }
+// function datetime() {
+//     alert("25.03.2024 12:25");
+// }
+
+// ====lesson 3 ===
+// let age;
+// age = 25;
+// console.log (age)
+
+let number1 = 10;
+let number2 = 5;
+let result = number1 + number2;
+console.log(result);
 
 // ===mmm===
 
-let temperature = 17;
-let isWindy = true;
+// let temperature = 17;
+// let isWindy = true;
 
-if (isWindy) {
-    console.log(Windy);
-}
-else {
-    console.log(There will be no wind);
-}
-if (temperature < 15) {
-    console.log(Dress warmly and take a hat);
-}
-else {
-    console.log(A good day for a walk);
-}
+// if (isWindy) {
+//     console.log(Windy);
+// }
+// else {
+//     console.log(There will be no wind);
+// }
+// if (temperature < 15) {
+//     console.log(Dress warmly and take a hat);
+// }
+// else {
+//     console.log(A good day for a walk);
+// }
