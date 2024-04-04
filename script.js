d0edfe145571e638b0bcce3e241874c87ec0f85f
HelloWorld <dessau505@gmail.com>
2024-04-04 10:00:16 +0300

    lesson 5

--- script.js
+++ script.js
@@ -17,18 +17,29 @@
 
 // ===lesson4===
 
-let temperature = 17;
-let isWindy = true;
+// let temperature = 17;
+// let isWindy = true;
 
-if (isWindy) {
-    console.log("Windy");
-}
-else {
-    console.log("There will be no wind");
-}
-if (temperature < 15) {
-    console.log("Dress warmly and take a hat");
-}
-else {
-    console.log("A good day for a walk");
-}
+// if (isWindy) {
+//     console.log("Windy");
+// }
+// else {
+//     console.log("There will be no wind");
+// }
+// if (temperature < 15) {
+//     console.log("Dress warmly and take a hat");
+// }
+// else {
+//     console.log("A good day for a walk");
+// }
+
+// ====Lesson5======
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// =====Lesson6=====
+let vegetables = ["carrot","potato","cucumber"];
+vegetables.push("tomato");
+
+console.log(vegetables);
